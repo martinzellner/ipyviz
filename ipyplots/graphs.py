f4ea1454e5482acac4e8ad1df1c7d21b396d01b9
Martin Zellner <martin.zellner@martinzellner.eu>
2016-06-16 00:26:15 +0200

    docstrings and type hints

--- ipyplots/graphs.py
+++ ipyplots/graphs.py
@@ -5,24 +5,30 @@ from IPython.display import HTML
 from string import Template
 import os
 import sys
-from IPython.display import display_javascript, Javascript
+import numpy
+import networkx
 
-
-def graph_plot(adjacency_matrix):
+def graph_plot(adjacency_matrix: numpy.ndarray):
     """
     Creates a JavaScript Graph Plot from the graph given by the adjacency matrix
-    :param adjacency_matrix: A numpy array returning the adjacency matrix
-    :return: an ipython html object
+
+    :param adjacency_matrix: The adjacency matrix of the graph to be plotted
+    :type adjacency_matrix: :class:`numpy.ndarray`
+    :return: The IPython HTML output
+    :rtype: :class:`IPython.display.HTML`
     """
     g = nx.from_numpy_matrix(adjacency_matrix)
     return nx_graph_plot(g)
 
 
-def nx_graph_plot(nx_graph):
+def nx_graph_plot(nx_graph: networkx.classes.Graph):
     """
     Creates a JavaScript Graph Plot from the given networkx graph
-    :param nx_graph: A networkx graph object
-    :return: an ipython html object
+
+    :param nx_graph: The graph to be plotted
+    :type nx_graph: :func:`networkx.Graph`
+    :return: The IPython HTML output
+    :rtype: :class:`IPython.display.HTML`
     """
     data = json_graph.node_link_data(nx_graph)
     s = json.dumps(data)
